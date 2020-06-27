5502631ef8bc4b468577d57b9f017fb4408b2c09
Zackattak01 <zbroderson@hotmail.com>
2020-06-27 19:05:30 -0400

    Revert "Remove bounds checking"
    This reverts commit 84575697e402dc517a3101b02326fed27d9a9349.

--- BuildProcess.cs
+++ BuildProcess.cs
@@ -19,6 +19,8 @@ namespace BrainfuckCompiler
 
 			List<string> cStatements = TranslateCommandsToC(commands);
 
+			//write optimization code here
+
 			WriteCToFile(cStatements);
 
 			CompileC();
@@ -117,9 +119,11 @@ namespace BrainfuckCompiler
 				{
 					case '>':
 						cCode.Add("i++;");
+						cCode.Add("if(i > tape + 30000){i=tape + 30000;}");
 						break;
 					case '<':
 						cCode.Add("i--;");
+						cCode.Add("if(i < tape){i=tape;}");
 						break;
 					case '+':
 						cCode.Add("(*i)++;");
@@ -182,13 +186,22 @@ namespace BrainfuckCompiler
 				bool breakScope = false;
 
 				//catch all for anything non-optimizable
-				if (statement.Contains("f") || statement.Contains("get")
-					|| statement.Contains("while") || statement.Contains("}"))
+				if (statement != "if(i > tape + 30000){i=tape + 30000;}" && statement != "if(i < tape){i=tape;}")
 				{
-					breakScope = true;
-				}
+					if (statement.Contains("f") || statement.Contains("get")
+						|| statement.Contains("while") || statement.Contains("}"))
+					{
+						breakScope = true;
+					}
 
-				
+				}
+				else if(statement == "if(i > tape + 30000){i=tape + 30000;}" || statement == "if(i < tape){i=tape;}")
+					continue;
+
+
+
+
+
 				//now we can check for optimizability
 				if (statement == previousOptimizableStatement && !breakScope)
 					consecutiveStatements++;
@@ -228,11 +241,7 @@ namespace BrainfuckCompiler
 				previousOptimizableStatement = statement; 
 			}
 
-			//the foreach wont catch the last statment in the list and since we know it always will be the ending '}' for the main function we can just add it here
 			optimizedCode.Add("}");
-
-			
-
 			return optimizedCode;
 		}
 
